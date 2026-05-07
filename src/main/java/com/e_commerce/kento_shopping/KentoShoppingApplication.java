8d37ca5fbeb052f1a88f81eb69566c87d3048943
kietTD <trandanhkiet160903@gmail.com>
2026-05-07 10:56:37 +0700

    chore: initialize Spring Boot project

---
+++ src/main/java/com/e_commerce/kento_shopping/KentoShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.e_commerce.kento_shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KentoShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KentoShoppingApplication.class, args);
+	}
+
+}
