f6b0a6e2557873d6a283fa90b538976c38e0d49a
kietTD <trandanhkiet160903@gmail.com>
2026-05-16 15:53:00 +0700

    branch feature/cart done

---
+++ src/main/java/com/e_commerce/kento_shopping/exception/InsufficientStockException.java
@@ -0,0 +1,7 @@
+package com.e_commerce.kento_shopping.exception;
+
+public class InsufficientStockException extends RuntimeException {
+    public InsufficientStockException(String message) {
+        super(message);
+    }
+}
