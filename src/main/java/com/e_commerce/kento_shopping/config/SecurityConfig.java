9615698662d612f92036df76e4522a640826db47
kietTD <trandanhkiet160903@gmail.com>
2026-05-20 15:59:57 +0700

    clear branch

--- src/main/java/com/e_commerce/kento_shopping/config/SecurityConfig.java
+++ src/main/java/com/e_commerce/kento_shopping/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
         http
             .cors(cors -> cors.configurationSource(request -> {
                 CorsConfiguration config = new CorsConfiguration();
-                config.setAllowedOrigins(List.of("http://localhost:3000")); // your frontend URL
+                config.setAllowedOrigins(List.of("http://localhost:5173"));
                 config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
                 config.setAllowedHeaders(List.of("*"));
                 config.setAllowCredentials(true);
